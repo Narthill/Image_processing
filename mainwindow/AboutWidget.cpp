6981c084d790c07c93c17dda492669e26792a0a6
Chen Hao <121972390@qq.com>
2018-09-06 12:25:40 +0800

    修改源码仓库链接

--- mainwindow/AboutWidget.cpp
+++ mainwindow/AboutWidget.cpp
@@ -7,14 +7,14 @@ AboutWidget::AboutWidget(QWidget *parent)
 	ui = new Ui::AboutWidget;
 	ui->setupUi(this);
 
-	ui->label->setText("<html>" 
-		+ tr("<b>QT版本:</b>") + "<br>"
+	ui->label->setText(
+		tr("<b>QT版本:</b>") + "<br>"
 		+ tr("QT5.9.4 32bit") + "<br>" 
 		+ tr("<b>OPENCV版本:</b>") + "<br>"
 		+ tr("opencv 2.4.10 32bit") + "<br>" 
 		+ tr("<b>源码地址:</b>") + "<br>"
-		+ tr("https://github.com/maiarChen/Image_processing") + "</html>");
-
+		+ "<a href=\"https://github.com/Narthill/Image_processing\">https://github.com/Narthill/Image_processing</a>");
+	ui->label->setOpenExternalLinks(true);
 	QObject::connect(ui->closeBtn, &QPushButton::clicked, this, &QWidget::close);
 }
 
